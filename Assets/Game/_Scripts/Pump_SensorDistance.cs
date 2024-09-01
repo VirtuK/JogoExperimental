f22c34cc9cf4cc419b7cbb19cd68b658dcef98b0
potatsts <yeppotatinhas@gmail.com>
2024-09-01 01:05:44 -0300

    organizei o projeto
    organizei os arquivos e as pastas,
    deletei arquivos inúteis

---
+++ Assets/Game/_Scripts/Pump_SensorDistance.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Uduino;
+
+public class Pump_SensorDistance : MonoBehaviour
+{
+    float distance = 0;
+
+    //------------------------------------\\
+
+    void Start()
+    {
+        UduinoManager.Instance.OnDataReceived += DataReceived;
+    }
+    //
+    void DataReceived(string data, UduinoDevice board)
+    {
+        bool ok = float.TryParse(data, out float d);
+
+        if (ok)
+        {
+            if(d > 0) distance = d;
+        } else
+        {
+            Debug.Log("Error parsing " + data);
+        }
+    }
+    //
+    public float GetDistance(){
+        return distance;
+    }
+}
+/*
+Controle bomba de ar:
+
+Distância mínima (fechada) - <= 5cm
+Distância máxima (aberta) - >= 44cm
+*/
