97a148033eb432e341ba57abf9903d328adbb89b
VirtuK <vitorkuiava.vc@gmail.com>
2024-09-05 11:01:10 -0300

    Os mapas tão saindo

--- Assets/Game/_Scripts/Minefield Scripts/Grid.cs
+++ Assets/Game/_Scripts/Minefield Scripts/Grid.cs
@@ -7,13 +7,14 @@ using UnityEngine.VFX;
 
 public class Grid : MonoBehaviour
 {
-    public int width;
-    public int lenght;
+    private int width = 16;
+    private int lenght = 16;
     public int bombCount;
     public GameObject tile;
     public List<GameObject[]> gridPosition = new List<GameObject[]>();
     public List<Vector3> pathPosition = new List<Vector3>();
     public List<GameObject> bombList = new List<GameObject>();
+    public int levelNumbers = 0;
     public Transform cam;
     public GameObject player;
     public Vector3 start;
@@ -21,51 +22,38 @@ public class Grid : MonoBehaviour
     public bool startDecided = false;
     public GameObject bombPrefab;
     public GameObject bombs;
+    List<Vector2> level0 = new List<Vector2>();
     // Start is called before the first frame update
     void Start()
     {
+        
+        
+        level0.AddRange(new List<Vector2> { new Vector2(1, 15), new Vector2(1,14), new Vector2(1,13),
+        new Vector2(1,12), new Vector2(2,12), new Vector2(3,12), new Vector2(4,12),
+        new Vector2(5,12), new Vector2(6,12), new Vector2(6,11), new Vector2(6,10),
+        new Vector2(5,10), new Vector2(5,9), new Vector2(5,8), new Vector2(5,7),
+        new Vector2(5,6), new Vector2(5,5), new Vector2(4,5), new Vector2(3,5),
+        new Vector2(2,5), new Vector2(1,5), new Vector2(1,4), new Vector2(1,3),
+        new Vector2(1,2), new Vector2(1,1), new Vector2(2,1), new Vector2(3,1),
+        new Vector2(4,1), new Vector2(5,1), new Vector2(5,2), new Vector2(5,3),
+        new Vector2(6,3), new Vector2(7,3), new Vector2(7,4), new Vector2(8,4),
+        new Vector2(9,4), new Vector2(9,5), new Vector2(9,6), new Vector2(9,7),
+        new Vector2(9,8), new Vector2(9,9), new Vector2(9,10), new Vector2(9,11),
+        new Vector2(9,12), new Vector2(9,13), new Vector2(10,13), new Vector2(11,13),
+        new Vector2(11,12), new Vector2(11,11), new Vector2(11,10), new Vector2(12,10), new Vector2(12,9),
+        new Vector2(12,8), new Vector2(12,7), new Vector2(13,7), new Vector2(13,6),
+        new Vector2(13,5), new Vector2(13,4), new Vector2(14,4), new Vector2(14,3),
+        new Vector2(14,2), new Vector2(14,1), new Vector2(14,0)});
 
         for (int i = 0; i <= width; i++)
         {
             List<GameObject> gridX = new List<GameObject>();
             for (int j = 0; j <= lenght; j++)
             {
-                GameObject grid = Instantiate(tile, new Vector3(i, j), Quaternion.identity);
+                GameObject grid = Instantiate(tile, new Vector3(i, j, 0), Quaternion.identity);
                 grid.transform.SetParent(this.transform);
                 gridX.Add(grid);
-                if (i == 0 || i == width || j == 0 || j == width)
-                {
-                    if (i != 0 && j != 0 || i != width && j != 0 || i != width && j != lenght || i != 0 && j != lenght)
-                    {
-                        int x = Random.Range(1, width);
-                        if (x == 1 && !finishDecided)
-                        {
-                            setFinishPoint(grid);
-
-                        }
-                        else
-                        {
-                            setWall(grid);
-                        }
-                    }
-                    else
-                    {
-                        setWall(grid);
-                    }
-                }
-                else
-                {
-                    
-                    int x = Random.Range(1, 2);
-                    if(x == 1)
-                    {
-                        setPath(grid);
-                    }
-                    else
-                    {
-                        setWall(grid);
-                    }
-                }
+                setWall(grid);
 
                 
             }
@@ -74,13 +62,7 @@ public class Grid : MonoBehaviour
         }
         
         cam.transform.position = new Vector3((float)width / 2 - 0.5f, (float)lenght / 2 - 0.5f, -1.5f);
-        setPlayer();
-        setBombs();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        setPath();
         
     }
     void setPlayer()
@@ -102,17 +84,43 @@ public class Grid : MonoBehaviour
         finishDecided = true;
     }
 
-    void setPath(GameObject grid)
+    void setPath()
     {
-        grid.name = "Path";
-        grid.GetComponent<SpriteRenderer>().color = Color.white;
-        if (!startDecided)
+        int rand = 0;
+        switch (rand)
         {
-            start = grid.transform.position;
-            startDecided = true;
+            case 0:
+                for (int i = 0; i < level0.Count; i++)
+                {
+                    int x = (int)level0[i].x;
+                    int y = (int)level0[i].y;
+                    if (i == 0)
+                    {
+                        setStart(gridPosition[x][y]);
+                    }
+                    else if (i == level0.Count - 1)
+                    {
+                        setFinishPoint(gridPosition[x][y]);
+                    }
+                    else
+                    {
+                        gridPosition[x][y].name = "Path";
+                        gridPosition[x][y].GetComponent<SpriteRenderer>().color = Color.white;
+                    }
+                    pathPosition.Add(gridPosition[x][y].transform.position);
+                }
+                break;
         }
-        pathPosition.Add(grid.transform.position);
-        
+        setPlayer();
+        setBombs();
+    }
+
+    void setStart(GameObject grid)
+    {
+        grid.name = "Start";
+        grid.GetComponent<SpriteRenderer>().color = Color.yellow;
+        start = grid.transform.position;
+
     }
 
     void setBombs()
